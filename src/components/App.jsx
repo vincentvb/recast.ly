1496c55a6ca6edc9db1ac7f1509ebb54b4893be7
Hack Reactor Student <student@hackreactor.com>
2017-04-12 19:52:09 -0700

    Some tests passing

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,33 @@
-var App = () => (
-  <div>
-    <Nav />
-    <div className="col-md-7">
-      <VideoPlayer/>
-    </div>
-    <div className="col-md-5">
-      <VideoList/>
-    </div>
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+      currentVideo: this.props.videos === undefined ? window.exampleVideoData[0] || this.props.videos[0],
+      videoList: this.props.videos === undefined ? window.exampleVideoData[0] || this.props.videos[0]
+    }
+    console.log(this.state.currentVideo)
+  }
+
+
+  changeCurrentVideo(newVideo) {
+    this.setState({currentVideo: newVideo})
+  }
+  render() {
+    return(
+      <div>
+        <Nav />
+        <div className="col-md-7">
+          <VideoPlayer video={this.state.currentVideo} state={this.state}/>
+        </div>
+        <div className="col-md-5">
+          <VideoList videos={this.state.videoList} state={this.state}/>
+        </div>
+      </div>
+    )
+  }
+};
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-// TODO: Render the `App` component to the DOM
+ReactDOM.render(<App videos={window.exampleVideoData} />, document.getElementById('app'))
 
